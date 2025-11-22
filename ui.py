4630e9cc2dd67297aa182d25fbd9a5083bf469ba
Lazaros Bistas <lazaros120@gmail.com>
2025-11-22 23:57:43 +0200

    added ui .py

--- ui.py
+++ ui.py
@@ -1,3 +1,24 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
+root = Tk(screenName="CALC_V2", )
 
+w = Label(root, text='CALC IS SHORT FOR CALCULATOR BTW')
+w.pack()
+
+text = StringVar()
+text.set("0")
+e1 = Entry(root)
+e2 = Entry(root)
+e1.insert(0, "First number:")
+#e2.insert(0, "Second number:")
+e1.pack()
+e2.pack()
+
+r = Button(root, text="exit", command=root.destroy)
+r.pack()
+
+progress_bar = ttk.Progressbar(root, orient='horizontal', length=100, mode='determinate')
+progress_bar.pack()
+
+root.mainloop()
